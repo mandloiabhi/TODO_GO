55fbddd2f123182def996d6dee1eaca45eae5a4b
Abhijeet <abhijeet291270@gmail.com>
2024-07-29 17:20:21 +0530

    Intial commit

--- main.go
+++ main.go
@@ -3,13 +3,47 @@ package main
 import (
 	"log"
 	"net/http"
+	"database/sql"
 	
-
+     "fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	 _ "github.com/lib/pq"
+	
+	
 )
-
+const (
+    host     = "localhost"
+    port     = 5432
+    user     = "postgres"
+    password = "postgres"
+    dbname   = "blogator"
+)
+type apiConfig struct {
+	DB *database.Queries
+}
 func main(){
+    psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+         
+        // open database
+	
+	//var db *sql.DB
+
+	// opens connection to a database
+
+    db, err := sql.Open("postgres", psqlconn)
+	if err != nil{
+        log.Printf("Error connecting: %s", err)
+	}
+	if err = db.Ping(); err != nil {
+		panic(err)
+		}
+	dbQueries := database.New(db)	
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
+
+    defer db.Close()
 
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
@@ -28,9 +62,14 @@ func main(){
 		Handler: router,
 	}
 
-	log.Printf("Serving on port: 8080" )
-	v1Router.Get("/healthz", handlerReadiness)
+	
 	router.Mount("/v1", v1Router)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerUsersCreate)
+
+	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
+	
 	log.Fatal(srv.ListenAndServe())
 
 
